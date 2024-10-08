07cf76c816dcf2df8eb99805d5ee31e61d72261c
Karan Thakkar <147543570+Karan1707@users.noreply.github.com>
2024-10-08 17:32:47 +0530

    Delete src/main/java/org/example directory

--- src/main/java/org/example/TransitDataBundle.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.util.List;
-import java.util.Map;
-
-public class TransitDataBundle {
-    public List<String> columns;
-    public List<Map<String, Object>> data;
-}
